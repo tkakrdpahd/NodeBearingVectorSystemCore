4138d06d6c5ce9323b4065f5ae62f8e8a663f4d7
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-12 18:52:19 -0500

    Refactoring Start
    Refactoring Start

--- main.cpp
+++ main.cpp
@@ -3,99 +3,11 @@
  * Security: Top Secret
  * Author: Minseok Doo
  * Date: Oct 13, 2024
- * Last Modified: Nov 11, 2024
+ * Last Modified: Nov 12, 2024
  */
 
-#define GL_SILENCE_DEPRECATION
-
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <thread>
-
-#include <GLFW/glfw3.h>
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-#include "Vector3.h"
-#include "CoordinateConverter.h"
-#include "NodeVector.h"
-#include "BearingVector.h"
-#include "LinerSegment.h"
-#include "AttributesManager.h"
-#include "SocketServer.h"
 
 int main() {
-    // GLFW 초기화
-    if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW\n";
-        return -1;
-    }
-
-    // OpenGL 버전 설정 (3.3 core profile)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    // GLFW 윈도우 생성
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL Example", NULL, NULL);
-    if (window == NULL) {
-        std::cerr << "Failed to create GLFW window\n";
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_None;                  // No docking functionality
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
-
-    // 메인 루프
-    while (!glfwWindowShouldClose(window)) {
-        // Poll and handle events (inputs, window resize, etc.)
-        glfwPollEvents();
-
-        // Start the ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-
-        // ImGui 데모 창 표시
-        ImGui::ShowDemoWindow();
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        // Swap buffers
-        glfwSwapBuffers(window);
-    }
-
-    // 정리 작업
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
-
     return 0;
 }
