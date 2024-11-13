28bd0b0dcbe16d58dc54674e6510c7ed91a29b55
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-13 08:47:54 -0500

    Update main.cpp
    Add comment

--- main.cpp
+++ main.cpp
@@ -9,5 +9,19 @@
 #include <iostream>
 
 int main() {
+    // Create SceneManager instance; std::shared_ptr
+        // Load operators modules
+    // Create ObjectManager instance refer to SceneManager instance
+        // Load entities modules
+        // Load vectors modules
+        // Load segments modules
+    // Create ServerManager instnace refer to SceneManager instance
+        // ServerManager load SocketManager and EventManager
+            // SocketManager load Socket Server and return Socket Number
+            // EventManager load API
+        // ServerManager instance return listenForClients();
+    // Call interfaces
+        // Call menu
+        // Call simulator
     return 0;
 }
