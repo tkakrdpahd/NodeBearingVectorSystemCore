b5bade26236c7990c60bdc843b84412200479d9b
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-13 09:20:52 -0500

    fix cmake bug
    add imgui header file

---
+++ modules/vectors/bearingVector.h
@@ -0,0 +1,21 @@
+/** 
+ * bearingVector.h
+ * Linked file: bearingVector.cpp
+ * Security: Top Secret
+ * Author: Minseok Doo
+ * Date: Oct 7, 2024
+ * Last Modified: Nov 11, 2024
+ * 
+ * Purpose of Class:
+ * Declare Node Vector
+ * 
+ * Equations:
+ * Equ(1): \vec{N_i} = \left( r_i, \theta_i, \phi_i \right)
+ */
+
+#ifndef BEARINGVECTOR_H
+#define BEARINGVECTOR_H
+
+#include "Vector3.h"
+
+#endif // BEARINGVECTOR_H
