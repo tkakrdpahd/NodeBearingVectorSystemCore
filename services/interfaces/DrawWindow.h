8f926cc5536531fc76032b3bec4992e663747837
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-14 12:28:35 -0500

    Menu interface separating (TESTED)
    Menu interface separating

--- services/interfaces/DrawWindow.h
+++ services/interfaces/DrawWindow.h
@@ -3,6 +3,7 @@
  */
 
 #include "LoadInterface.h" // Load Vulkan & SDL2 func
+#include "Menu.h"
 
 int interface(int, char**) {
     // Setup SDL
@@ -98,6 +99,8 @@ int interface(int, char**) {
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+    int counter = 0;        // 추가된 변수
+    float f = 0.0f;         // 추가된 변수
 
     // Main loop
     bool done = false;
@@ -139,43 +142,8 @@ int interface(int, char**) {
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        // if (show_demo_window)
-        //     ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-        // {
-        //     static float f = 0.0f;
-        //     static int counter = 0;
-
-        //     ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        //     ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        //     ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        //     ImGui::Checkbox("Another Window", &show_another_window);
-
-        //     ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-        //     ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-        //     if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-        //         counter++;
-        //     ImGui::SameLine();
-        //     ImGui::Text("counter = %d", counter);
-
-        //     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-        //     ImGui::End();
-        // }
-
-        // 3. Show another simple window.
-        // if (show_another_window)
-        // {
-        //     ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        //     ImGui::Text("Hello from another window!");
-        //     if (ImGui::Button("Close Me"))
-        //         show_another_window = false;
-        //     ImGui::End();
-        // }
-
+        menu(show_demo_window, show_another_window, clear_color, io, counter, f);
+        
         // Rendering
         ImGui::Render();
         ImDrawData* draw_data = ImGui::GetDrawData();
--- services/interfaces/menu.h
+++ services/interfaces/menu.h
@@ -5,3 +5,12 @@
  * Create Main Menu GUI
  */
 
+#ifndef MENU_H
+#define MENU_H
+
+#include <imgui.h>
+
+// 필요한 변수들을 매개변수로 전달
+void menu(bool& show_demo_window, bool& show_another_window, ImVec4& clear_color, ImGuiIO& io, int& counter, float& f);
+
+#endif // MENU_H
