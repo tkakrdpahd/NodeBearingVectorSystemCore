065d418e81b5349af5642a777f89e50ea7126892
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-16 18:15:59 -0500

    Update CoordinateConverter.cpp

--- modules/operators/CoordinateConverter.cpp
+++ modules/operators/CoordinateConverter.cpp
@@ -1,23 +1,24 @@
 /**
  * CoordinateConverter.cpp
- * Linked file CoordinateConverter.h
+ * Linked file: CoordinateConverter.h
  * Security: Confidential
  * Author: Minseok Doo
  * Date: Oct 7, 2024
  */
 
 #include "CoordinateConverter.h"
+#include "Vector3.h"
 
 /**
- * @brief SphericalVector를 CartesianVector로 변환합니다.
+ * @brief Converts a SphericalVector to a CartesianVector.
  * 
  * Equations:
  * Equ(1): x = r * sin(phi) * cos(theta)
  * Equ(2): y = r * sin(phi) * sin(theta)
  * Equ(3): z = r * cos(phi)
  * 
- * @param sv 변환할 SphericalVector 객체.
- * @return CartesianVector 변환된 CartesianVector 객체.
+ * @param sv SphericalVector object to be converted.
+ * @return CartesianVector Converted CartesianVector object.
  */
 CartesianVector CoordinateConverter::sphericalToCartesian(const SphericalVector& sv) {
     float x = sv.r * std::sin(sv.phi) * std::cos(sv.theta);
@@ -27,15 +28,15 @@ CartesianVector CoordinateConverter::sphericalToCartesian(const SphericalVector&
 }
 
 /**
- * @brief CartesianVector를 SphericalVector로 변환합니다.
+ * @brief Converts a CartesianVector to a SphericalVector.
  * 
  * Equations:
  * Equ(1): r = sqrt(x^2 + y^2 + z^2)
  * Equ(2): theta = atan2(y, x)
  * Equ(3): phi = acos(z / r)
  * 
- * @param cv 변환할 CartesianVector 객체.
- * @return SphericalVector 변환된 SphericalVector 객체.
+ * @param cv CartesianVector object to be converted.
+ * @return SphericalVector Converted SphericalVector object.
  */
 SphericalVector CoordinateConverter::cartesianToSpherical(const CartesianVector& cv) {
     float r = std::sqrt(cv.x * cv.x + cv.y * cv.y + cv.z * cv.z);
