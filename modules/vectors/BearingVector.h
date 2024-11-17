583d9c607623388d3e7e28107934f83785818782
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-16 20:14:10 -0500

    Update BearingVector.h

--- modules/vectors/BearingVector.h
+++ modules/vectors/BearingVector.h
@@ -16,11 +16,13 @@
  * 
  * @param NodeIndex Patrent Node Vector Index
  * @param Force Bearing Vector Force; (x, y, z)
- * @param Vector Bearing Vector location; (x, y, z), (r_i, theta_i, phi_i)
+ * @param Vector Bearing Vector location; (x, y, z), (d_i, theta_i, phi_i)
  */
 struct BearingVector
 {
     int NodeIndex;
     Vector3 Force;
     Vector3 Vector;
+
+    BearingVector(int nodeId, const Vector3& force, const Vector3& vec) : NodeIndex(nodeId), Force(force), Vector(vec) {}
 };
