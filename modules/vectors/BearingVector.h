5db66a4ed5a0c1468e6b56810707a5128354e95e
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-17 10:57:20 -0500

    Update BearingVector (Tested)
    Update BearingVector (Tested)

--- modules/vectors/BearingVector.h
+++ modules/vectors/BearingVector.h
@@ -6,7 +6,16 @@
  * Last Modified: Nov 16, 2024
  * 
  * Purpose: Implementation of the BearingVector class methods
+ * 
+ * Equations
+ * Equ(1): (B_i ) ⃗=(■(sin⁡〖ϕ_i 〗  cos⁡〖θ_i 〗@sin⁡〖ϕ_i 〗  sin⁡〖θ_i 〗@cos⁡〖ϕ_i 〗 ))
+ * Equ(2): (B_(i,d) ) ⃗=(B_(i,d,x),B_(i,d,y),B_(i,d,z) )
+ * Equ(3): (F_i ) ⃗=F_(x_i ) x ̂+F_(y_i ) y ̂+F_(z_i ) z ̂
+ * Equ(4): R_i=R_z (θ_i ) R_y (ϕ_i )
+ * Equ(5): R_z (θ_i )=(■(cos⁡〖θ_i 〗&〖- sin〗⁡〖θ_i 〗&0@sin⁡〖θ_i 〗&cos⁡〖θ_i 〗&0@0&0&1))
+ * Equ(6): R_y (ϕ_i )=(■(cos⁡〖ϕ_i 〗&0&sin⁡〖ϕ_i 〗@0&1&0@-sin⁡〖ϕ_i 〗&0&cos⁡〖ϕ_i 〗 ))
  */
+
 #ifndef BEARINGVECTOR_H
 #define BEARINGVECTOR_H
 
@@ -17,7 +26,7 @@
 /**
  * @brief Bearing Vector
  * 
- * @param NodeVector Patrent Node Vector
+ * @param NodeVector Parent Node Vector
  * @param Force Bearing Vector Force; (x, y, z)
  * @param Vector Bearing Vector location; (x, y, z), (d_i, theta_i, phi_i)
  */
@@ -27,7 +36,12 @@ struct BearingVector
     Vector3 Force;
     Vector3 Vector;
 
-    BearingVector(const NodeVector& node, const Vector3& force, const Vector3& vec) : Node(node), Force(force), Vector(vec) {}
+    // constructor
+    BearingVector() : Node(0, Vector3()), Force(), Vector() {}
+
+    // 매개변수 생성자
+    BearingVector(const NodeVector& node, const Vector3& force, const Vector3& vec) 
+        : Node(node), Force(force), Vector(vec) {}
 };
 
 #endif // BEARINGVECTOR_H
