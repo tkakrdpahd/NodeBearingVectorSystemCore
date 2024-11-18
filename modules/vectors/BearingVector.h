ed0963e133de92dc803cee4ad33ba2a732e37396
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-18 13:27:32 -0500

    update bearing
    use nodevector

--- modules/vectors/BearingVector.h
+++ modules/vectors/BearingVector.h
@@ -8,39 +8,41 @@
  * Purpose: Implementation of the BearingVector class methods
  * 
  * Equations
- * Equ(1): (B_i ) ⃗=(■(sin⁡〖ϕ_i 〗  cos⁡〖θ_i 〗@sin⁡〖ϕ_i 〗  sin⁡〖θ_i 〗@cos⁡〖ϕ_i 〗 ))
+ * Equ(1): (B_i ) ⃗=(■(sin⁡ϕ_i cos⁡θ_i@sin⁡ϕ_i sin⁡θ_i@cos⁡ϕ_i ))
  * Equ(2): (B_(i,d) ) ⃗=(B_(i,d,x),B_(i,d,y),B_(i,d,z) )
  * Equ(3): (F_i ) ⃗=F_(x_i ) x ̂+F_(y_i ) y ̂+F_(z_i ) z ̂
  * Equ(4): R_i=R_z (θ_i ) R_y (ϕ_i )
- * Equ(5): R_z (θ_i )=(■(cos⁡〖θ_i 〗&〖- sin〗⁡〖θ_i 〗&0@sin⁡〖θ_i 〗&cos⁡〖θ_i 〗&0@0&0&1))
- * Equ(6): R_y (ϕ_i )=(■(cos⁡〖ϕ_i 〗&0&sin⁡〖ϕ_i 〗@0&1&0@-sin⁡〖ϕ_i 〗&0&cos⁡〖ϕ_i 〗 ))
+ * Equ(5): R_z (θ_i )=(■(cos⁡θ_i & -sin⁡θ_i & 0@sin⁡θ_i & cos⁡θ_i & 0@0 & 0 & 1))
+ * Equ(6): R_y (ϕ_i )=(■(cos⁡ϕ_i & 0 & sin⁡ϕ_i@0 & 1 & 0@-sin⁡ϕ_i & 0 & cos⁡ϕ_i ))
  */
 
 #ifndef BEARINGVECTOR_H
 #define BEARINGVECTOR_H
 
 #include "Vector3.h"
+#include "NodeVector.h"
 #include "CoordinateConverter.h"
 
 /**
  * @brief Bearing Vector
  * 
- * @param NodeIndex Parent Node Vector Index
+ * @param NodeVector Parent Node Vector
  * @param Force Bearing Vector Force; (x, y, z)
  * @param Vector Bearing Vector location; (x, y, z), (d_i, theta_i, phi_i)
  */
 struct BearingVector
 {
-    int NodeIndex;
+    NodeVector Node;
     Vector3 Force;
     Vector3 Vector;
 
     // 기본 생성자
-    BearingVector() : NodeIndex(0), Force(), Vector() {}
+    // NodeVector을 기본값으로 초기화
+    BearingVector() : Node(0, Vector3()), Force(), Vector() {}
 
     // 매개변수 생성자
-    BearingVector(int nodeIndex, const Vector3& force, const Vector3& vec) 
-        : NodeIndex(nodeIndex), Force(force), Vector(vec) {}
+    BearingVector(const NodeVector& node, const Vector3& force, const Vector3& vec) 
+        : Node(node), Force(force), Vector(vec) {}
 };
 
 #endif // BEARINGVECTOR_H
--- tests/modules/vectors/BearingVectorTest.cc
+++ tests/modules/vectors/BearingVectorTest.cc
@@ -8,6 +8,7 @@
 #include <gtest/gtest.h>
 #include "BearingVector.h"
 #include "Vector3.h"
+#include "NodeVector.h"
 #include <cmath>
 #include <iostream>
 
@@ -20,7 +21,7 @@ bool areVectorsEqual(const Vector3& v1, const Vector3& v2, double epsilon = 1e-6
 
 // 로그 출력 헬퍼 함수
 void logBearingVector(const BearingVector& bv) {
-    std::cout << "Node Index: " << bv.NodeIndex << std::endl;
+    std::cout << "Node Index: " << bv.Node.Index << std::endl;
     std::cout << "Force: (" << bv.Force.x << ", " << bv.Force.y << ", " << bv.Force.z << ")" << std::endl;
     std::cout << "Vector: (" << bv.Vector.x << ", " << bv.Vector.y << ", " << bv.Vector.z << ")" << std::endl;
 }
@@ -32,7 +33,7 @@ TEST(BearingVectorTest, DefaultConstructor) {
     std::cout << "BearingVectorTest.DefaultConstructor:" << std::endl;
     logBearingVector(bv);
 
-    EXPECT_EQ(bv.NodeIndex, 0);
+    EXPECT_EQ(bv.Node.Index, 0);
     EXPECT_TRUE(areVectorsEqual(bv.Force, Vector3()));
     EXPECT_TRUE(areVectorsEqual(bv.Vector, Vector3()));
 }
@@ -40,15 +41,17 @@ TEST(BearingVectorTest, DefaultConstructor) {
 // BearingVector 매개변수 생성자 테스트
 TEST(BearingVectorTest, ParameterizedConstructor) {
     int testIndex = 1;
-    Vector3 testForce(5.0, -3.0, 2.5);
-    Vector3 testVector(100.0, 45.0, 60.0);
+    Vector3 nodeVec(10.0f, 20.0f, 30.0f); // 예시 NodeVector의 Vector3 값
+    NodeVector node(testIndex, nodeVec);
+    Vector3 testForce(5.0f, -3.0f, 2.5f);
+    Vector3 testVector(100.0f, 45.0f, 60.0f);
 
-    BearingVector bv(testIndex, testForce, testVector);
+    BearingVector bv(node, testForce, testVector);
 
     std::cout << "BearingVectorTest.ParameterizedConstructor:" << std::endl;
     logBearingVector(bv);
 
-    EXPECT_EQ(bv.NodeIndex, testIndex);
+    EXPECT_EQ(bv.Node.Index, testIndex);
     EXPECT_TRUE(areVectorsEqual(bv.Force, testForce));
     EXPECT_TRUE(areVectorsEqual(bv.Vector, testVector));
 }
@@ -56,37 +59,41 @@ TEST(BearingVectorTest, ParameterizedConstructor) {
 // BearingVector 멤버 변수 수정 테스트
 TEST(BearingVectorTest, MemberVariableModification) {
     int initialIndex = 2;
-    Vector3 initialForce(7.5, -4.5, 3.0);
-    Vector3 initialVector(150.0, 60.0, 75.0);
+    Vector3 initialNodeVec(15.0f, 25.0f, 35.0f); // 예시 NodeVector의 Vector3 값
+    NodeVector node(initialIndex, initialNodeVec);
+    Vector3 initialForce(7.5f, -4.5f, 3.0f);
+    Vector3 initialVector(150.0f, 60.0f, 75.0f);
 
-    BearingVector bv(initialIndex, initialForce, initialVector);
+    BearingVector bv(node, initialForce, initialVector);
 
-    bv.NodeIndex = 3;
-    bv.Force = Vector3(10.0, -5.0, 4.0);
-    bv.Vector = Vector3(200.0, 75.0, 90.0);
+    bv.Node.Index = 3;
+    bv.Force = Vector3(10.0f, -5.0f, 4.0f);
+    bv.Vector = Vector3(200.0f, 75.0f, 90.0f);
 
     std::cout << "BearingVectorTest.MemberVariableModification:" << std::endl;
     logBearingVector(bv);
 
-    EXPECT_EQ(bv.NodeIndex, 3);
-    EXPECT_TRUE(areVectorsEqual(bv.Force, Vector3(10.0, -5.0, 4.0)));
-    EXPECT_TRUE(areVectorsEqual(bv.Vector, Vector3(200.0, 75.0, 90.0)));
+    EXPECT_EQ(bv.Node.Index, 3);
+    EXPECT_TRUE(areVectorsEqual(bv.Force, Vector3(10.0f, -5.0f, 4.0f)));
+    EXPECT_TRUE(areVectorsEqual(bv.Vector, Vector3(200.0f, 75.0f, 90.0f)));
 }
 
 // BearingVector 복사 생성자 테스트
 TEST(BearingVectorTest, CopyConstructor) {
     int originalIndex = 4;
-    Vector3 originalForce(12.0, -6.0, 5.0);
-    Vector3 originalVector(250.0, 90.0, 105.0);
+    Vector3 originalNodeVec(20.0f, 30.0f, 40.0f); // 예시 NodeVector의 Vector3 값
+    NodeVector originalNode(originalIndex, originalNodeVec);
+    Vector3 originalForce(12.0f, -6.0f, 5.0f);
+    Vector3 originalVector(250.0f, 90.0f, 105.0f);
 
-    BearingVector originalBV(originalIndex, originalForce, originalVector);
+    BearingVector originalBV(originalNode, originalForce, originalVector);
 
     BearingVector copiedBV(originalBV);
 
     std::cout << "BearingVectorTest.CopyConstructor:" << std::endl;
     logBearingVector(copiedBV);
 
-    EXPECT_EQ(copiedBV.NodeIndex, originalBV.NodeIndex);
+    EXPECT_EQ(copiedBV.Node.Index, originalBV.Node.Index);
     EXPECT_TRUE(areVectorsEqual(copiedBV.Force, originalBV.Force));
     EXPECT_TRUE(areVectorsEqual(copiedBV.Vector, originalBV.Vector));
 }
@@ -94,10 +101,12 @@ TEST(BearingVectorTest, CopyConstructor) {
 // BearingVector 대입 연산자 테스트
 TEST(BearingVectorTest, AssignmentOperator) {
     int originalIndex = 5;
-    Vector3 originalForce(15.0, -7.5, 6.0);
-    Vector3 originalVector(300.0, 120.0, 135.0);
+    Vector3 originalNodeVec(25.0f, 35.0f, 45.0f); // 예시 NodeVector의 Vector3 값
+    NodeVector originalNode(originalIndex, originalNodeVec);
+    Vector3 originalForce(15.0f, -7.5f, 6.0f);
+    Vector3 originalVector(300.0f, 120.0f, 135.0f);
 
-    BearingVector originalBV(originalIndex, originalForce, originalVector);
+    BearingVector originalBV(originalNode, originalForce, originalVector);
 
     BearingVector assignedBV;
     assignedBV = originalBV;
@@ -105,7 +114,7 @@ TEST(BearingVectorTest, AssignmentOperator) {
     std::cout << "BearingVectorTest.AssignmentOperator:" << std::endl;
     logBearingVector(assignedBV);
 
-    EXPECT_EQ(assignedBV.NodeIndex, originalBV.NodeIndex);
+    EXPECT_EQ(assignedBV.Node.Index, originalBV.Node.Index);
     EXPECT_TRUE(areVectorsEqual(assignedBV.Force, originalBV.Force));
     EXPECT_TRUE(areVectorsEqual(assignedBV.Vector, originalBV.Vector));
 }
