8cf16be5766e5c254e0a406abf685c4504c7fde6
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-18 17:33:15 -0500

    remove all server component
    remove all server component

--- main.cpp
+++ main.cpp
@@ -9,24 +9,23 @@
 #include <memory>
 #include <thread>
 #include "DrawWindow.h"
-#include "ServerManager.h"
 
 int main(int argc, char** argv) {
-    ServerManager serverManager;
+    // ServerManager serverManager;
 
     // 서버를 별도 스레드에서 실행
-    std::thread serverThread([&serverManager]() {
-        serverManager.start();
-    });
+    // std::thread serverThread([&serverManager]() {
+    //     serverManager.run();
+    // });
 
     // 인터페이스 실행
     interface(argc, argv);
 
     // 인터페이스 종료 후 서버 정리
-    serverManager.stop();
-    if (serverThread.joinable()) {
-        serverThread.join();
-    }
+    // serverManager.shutdown();
+    // if (serverThread.joinable()) {
+    //     serverThread.join();
+    // }
 
     return 0;
 }
--- services/interfaces/Menu.cpp
+++ services/interfaces/Menu.cpp
@@ -6,7 +6,6 @@
 #include <memory>
 
 #include "Menu.h"
-#include "SocketManager.h"
 
 void menu(bool& show_demo_window, bool& show_another_window, ImVec4& clear_color, ImGuiIO& io, int& counter, float& f) {
 
