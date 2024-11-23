f4296abc232a1dfbfeae56c26f362a6c93822eb4
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-23 10:09:04 -0500

    Update

---
+++ services/managers/SegmentManager.h
@@ -0,0 +1,21 @@
+/**
+ * SegmentManager.h
+ * Linked File: SegmentManager.cpp
+ * Security: Top Secret
+ * Author: Minseok Doo
+ * Date: Nov 23, 2024
+ * Last Modified: Nov 23, 2024
+ * 
+ * Purpose: 
+ * Manage LinearSegment, SurfaceSegment and Index Buffer
+ * 
+ */
+
+#ifndef SCENEMANAGER_H
+#define SCENEMANAGER_H
+
+#include "IndexBuffer.h"
+#include "LinearSegment.h"
+#include "SurfaceSegment.h"
+
+#endif // SCENEMANAGER_H
