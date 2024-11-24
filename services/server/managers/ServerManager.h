ff07eb4b85491383ed3995f39ba49aa9f052e0ae
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-23 19:23:13 -0500

    Server Update
    Server Update

---
+++ services/server/managers/ServerManager.h
@@ -0,0 +1,29 @@
+/**
+ * ServerManager.h
+ */
+
+#ifndef SERVERMANAGER_H
+#define SERVERMANAGER_H
+
+#include "SocketManager.h"
+#include "RoutesManager.h"
+
+class ServerManager
+{
+private:
+    /* data */
+public:
+    ServerManager(/* args */);
+    ~ServerManager();
+};
+
+ServerManager::ServerManager(/* args */)
+{
+}
+
+ServerManager::~ServerManager()
+{
+}
+
+
+#endif // SERVERMANAGER_H
