beded84cd8383cfc7353ea49c402342e49e8b4eb
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-26 08:55:54 -0500

    Add bearingvector json logic (TESTED)

--- src/modules/entities/BearingVector.h
+++ src/modules/entities/BearingVector.h
@@ -10,7 +10,7 @@
  * Equations
  * Equ(1): (B_i ) ⃗=(■(sin⁡ϕ_i cos⁡θ_i@sin⁡ϕ_i sin⁡θ_i@cos⁡ϕ_i ))
  * Equ(2): (B_(i,d) ) ⃗=(B_(i,d,x),B_(i,d,y),B_(i,d,z) )
- * Equ(3): (F_i ) ⃗=F_(x_i ) x ̂+F_(y_i ) y ̂+F_(z_i ) z ̂
+ * Equ(3): (F_i ) ⃗=F_(x_i ) x ̂+F_(y_i ) y ̂+F_(z_i ) z ̂
  * Equ(4): R_i=R_z (θ_i ) R_y (ϕ_i )
  * Equ(5): R_z (θ_i )=(■(cos⁡θ_i & -sin⁡θ_i & 0@sin⁡θ_i & cos⁡θ_i & 0@0 & 0 & 1))
  * Equ(6): R_y (ϕ_i )=(■(cos⁡ϕ_i & 0 & sin⁡ϕ_i@0 & 1 & 0@-sin⁡ϕ_i & 0 & cos⁡ϕ_i ))
@@ -30,24 +30,65 @@ using json = nlohmann::json;
  * @brief Bearing Vector
  * 
  * @param NodeVector Parent Node Vector
- * @param Force Bearing Vector Force; (x, y, z)
+ * @param Force Bearing Vector Force in Cartesian coordinates (x, y, z)
  * @param Vector Bearing Vector location; (x, y, z), (d_i, theta_i, phi_i)
  */
 struct BearingVector
 {
     NodeVector Node;
-    Vector3 Force;
+    Vector3 Force;    // Always in Cartesian coordinates (x, y, z)
     Vector3 Vector;
 
-    // Constructor
+    /**
+     * @brief Constructs a BearingVector with a parent node, force, and position
+     * @param node Parent NodeVector
+     * @param force Force vector in Cartesian coordinates (x, y, z)
+     * @param vec Position vector (x, y, z) or (d_i, theta_i, phi_i)
+     */
     BearingVector(const NodeVector& node, const Vector3& force, const Vector3& vec) 
         : Node(node), Force(force), Vector(vec) {}
 
-    // JSON Serialization
-    json toJson() const;
-    static BearingVector fromJson(const json& j);
+    /**
+     * @brief Serializes the BearingVector to JSON format
+     * @return JSON object containing the bearing vector data
+     * @note Force is always stored in Cartesian coordinates
+     */
+    json toJson() const {
+        return {
+            {"node", Node.toJson()},
+            {"force", {           // Force in Cartesian coordinates
+                {"x", Force.x},
+                {"y", Force.y},
+                {"z", Force.z}
+            }},
+            {"vector", Vector.toJson()}
+        };
+    }
+
+    /**
+     * @brief Creates a BearingVector from JSON data
+     * @param j JSON object containing bearing vector data
+     * @return Constructed BearingVector instance
+     * @note Force is expected to be in Cartesian coordinates
+     */
+    static BearingVector fromJson(const json& j) {
+        return BearingVector(
+            NodeVector::fromJson(j["node"]),
+            Vector3(                      // Force in Cartesian coordinates
+                j["force"]["x"].get<float>(),
+                j["force"]["y"].get<float>(),
+                j["force"]["z"].get<float>()
+            ),
+            Vector3::fromJson(j["vector"])
+        );
+    }
     
-    // 출력 연산자 오버로드
+    /**
+     * @brief Overloaded output operator for string representation
+     * @param os Output stream
+     * @param bv BearingVector to output
+     * @return Reference to output stream
+     */
     friend std::ostream& operator<<(std::ostream& os, const BearingVector& bv) {
         os << "BearingVector(Node: " << bv.Node << ", Force: " << bv.Force << ", Vector: " << bv.Vector << ")";
         return os;
--- tests/modules/entities/BearingVectorTest.cc
+++ tests/modules/entities/BearingVectorTest.cc
@@ -34,3 +34,70 @@ TEST(BearingVectorTest, OutputOperator) {
     oss << bv;
     EXPECT_EQ(oss.str(), "BearingVector(Node: NodeVector(Index: 5, Vector: (1, 2, 3)), Force: (4, 5, 6), Vector: (7, 8, 9))");
 }
+
+TEST(BearingVectorTest, JsonSerialization) {
+    // Create initial object
+    Vector3 nodeVec(1.0f, 2.0f, 3.0f);
+    NodeVector node(5, nodeVec);
+    Vector3 force(4.0f, 5.0f, 6.0f);
+    Vector3 bearingVec(7.0f, 8.0f, 9.0f);
+    BearingVector original(node, force, bearingVec);
+
+    // Serialize to JSON
+    json j = original.toJson();
+
+    // Verify JSON structure
+    EXPECT_EQ(j["node"]["index"], 5);
+    EXPECT_FLOAT_EQ(j["node"]["vector"]["x"], 1.0f);
+    EXPECT_FLOAT_EQ(j["node"]["vector"]["y"], 2.0f);
+    EXPECT_FLOAT_EQ(j["node"]["vector"]["z"], 3.0f);
+    EXPECT_FLOAT_EQ(j["force"]["x"], 4.0f);
+    EXPECT_FLOAT_EQ(j["force"]["y"], 5.0f);
+    EXPECT_FLOAT_EQ(j["force"]["z"], 6.0f);
+    EXPECT_FLOAT_EQ(j["vector"]["x"], 7.0f);
+    EXPECT_FLOAT_EQ(j["vector"]["y"], 8.0f);
+    EXPECT_FLOAT_EQ(j["vector"]["z"], 9.0f);
+
+    // Deserialize
+    BearingVector deserialized = BearingVector::fromJson(j);
+
+    // Verify deserialized object
+    EXPECT_EQ(deserialized.Node.Index, original.Node.Index);
+    EXPECT_FLOAT_EQ(deserialized.Node.Vector.x, original.Node.Vector.x);
+    EXPECT_FLOAT_EQ(deserialized.Node.Vector.y, original.Node.Vector.y);
+    EXPECT_FLOAT_EQ(deserialized.Node.Vector.z, original.Node.Vector.z);
+    EXPECT_FLOAT_EQ(deserialized.Force.x, original.Force.x);
+    EXPECT_FLOAT_EQ(deserialized.Force.y, original.Force.y);
+    EXPECT_FLOAT_EQ(deserialized.Force.z, original.Force.z);
+    EXPECT_FLOAT_EQ(deserialized.Vector.x, original.Vector.x);
+    EXPECT_FLOAT_EQ(deserialized.Vector.y, original.Vector.y);
+    EXPECT_FLOAT_EQ(deserialized.Vector.z, original.Vector.z);
+}
+
+TEST(BearingVectorTest, JsonSerializationString) {
+    Vector3 nodeVec(1.0f, 2.0f, 3.0f);
+    NodeVector node(5, nodeVec);
+    Vector3 force(4.0f, 5.0f, 6.0f);
+    Vector3 bearingVec(7.0f, 8.0f, 9.0f);
+    BearingVector bv(node, force, bearingVec);
+    
+    // Serialize to JSON string
+    json j = bv.toJson();
+    std::string jsonStr = j.dump();
+
+    // Deserialize from JSON string
+    json parsedJson = json::parse(jsonStr);
+    BearingVector deserialized = BearingVector::fromJson(parsedJson);
+
+    // Verify
+    EXPECT_EQ(deserialized.Node.Index, 5);
+    EXPECT_FLOAT_EQ(deserialized.Node.Vector.x, 1.0f);
+    EXPECT_FLOAT_EQ(deserialized.Node.Vector.y, 2.0f);
+    EXPECT_FLOAT_EQ(deserialized.Node.Vector.z, 3.0f);
+    EXPECT_FLOAT_EQ(deserialized.Force.x, 4.0f);
+    EXPECT_FLOAT_EQ(deserialized.Force.y, 5.0f);
+    EXPECT_FLOAT_EQ(deserialized.Force.z, 6.0f);
+    EXPECT_FLOAT_EQ(deserialized.Vector.x, 7.0f);
+    EXPECT_FLOAT_EQ(deserialized.Vector.y, 8.0f);
+    EXPECT_FLOAT_EQ(deserialized.Vector.z, 9.0f);
+}
