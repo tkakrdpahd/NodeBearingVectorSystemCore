0e0bee30d128e517c878cd61b5cc05753227e852
Minseok Doo <89321156+tkakrdpahd@users.noreply.github.com>
2024-11-26 09:12:48 -0500

    add managersto

---
+++ src/server/dto/ObjectManagerDTO.h
@@ -0,0 +1,13 @@
+/**
+ * ObjectManagerDTO.h   
+ * Author: Minseok Doo
+ * Date: 2024-11-26
+ * Description: ObjectManagerDTO class for handling ObjectManager data in JSON format
+ */
+
+#ifndef OBJECTMANAGERDTO_H
+#define OBJECTMANAGERDTO_H
+
+#include "ObjectManager.h"
+
+#endif // OBJECTMANAGERDTO_H
---
+++ src/server/dto/SceneManagerDTO.h
@@ -0,0 +1,21 @@
+/**
+ * SceneManagerDTO.h
+ * Author: Minseok Doo
+ * Date: 2024-11-26
+ * Description: SceneManagerDTO class for handling SceneManager data in JSON format
+ */
+
+#ifndef SCENEMANAGERDTO_H
+#define SCENEMANAGERDTO_H
+
+#include "SceneManager.h"
+
+class SceneManagerDTO {
+    private:
+        /* data */
+    public:
+        SceneManagerDTO(/* args */);
+        ~SceneManagerDTO();
+};
+
+#endif // SCENEMANAGERDTO_H
